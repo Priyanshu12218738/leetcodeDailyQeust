446338901ba30eb7bdef44dae356ed277b4abc7a
Priyanshu Singh <119563912+Priyanshu12218738@users.noreply.github.com>
2026-07-20 17:22:51 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (53.22%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -5,8 +5,8 @@ class Solution {
         int ans =0;
          
          for(int i=0; i<32; i++){
-             ans = ans << 1;
-             ans = ans | (n & 1);
+             
+             ans = (ans<<1) | (n & 1);
              n>>>=1;
          }
          return ans;
